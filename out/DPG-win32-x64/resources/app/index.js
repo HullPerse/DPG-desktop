f79d1a523b925e2f00c2fd236ef875af96288d60
HullPerse <alex31054@yandex.ru>
2023-10-12 01:29:12 +0300

    Initial electron app commit

---
+++ out/DPG-win32-x64/resources/app/index.js
@@ -0,0 +1,40 @@
+const { BrowserWindow, app } = require("electron");
+
+app.whenReady().then(() => {
+  const loadingWin = new BrowserWindow({
+    width: 400,
+    height: 400,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  loadingWin.loadFile('loading.html');
+
+  setTimeout(() => {
+    const win = new BrowserWindow({
+      minWidth: 1350,
+      minHeight: 920,
+      autoHideMenuBar: true,
+      spellcheck: true,
+      hiddenInMissionControl: true,
+      webPreferences: {
+        nodeIntegration: true,
+        webviewTag: true
+      }
+    });
+    win.loadURL(`https://26.74.125.39:3000/`);
+    
+    win.once('ready-to-show', () => {
+      loadingWin.destroy();
+      win.show();
+    });
+  }, 3000);
+
+  app.on("window-all-closed", () => {
+    app.quit();
+  });
+});
